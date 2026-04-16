a9a888f348da980f3c27e9b975b89859fe88431c
240979 <240979@vutbr.cz>
2026-04-16 15:55:24 +0200

    removed unused files

--- src/main/java/records/RecordPTR.java
+++
@@ -1,9 +0,0 @@
-package records;
-
-public class RecordPTR extends RecordCNAME {
-
-	public RecordPTR(byte[] rawMessage, int lenght, int startIndex) {
-		super(rawMessage, lenght, startIndex);
-	}
-
-}
